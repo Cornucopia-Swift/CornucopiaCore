ab486a21bda40f319b7208d89c6f4c8b46cb21c6
Dr. Michael Lauer <mickey@vanille-media.de>
2021-01-16 20:11:14 +0100

    Storage/ExtendedFileAttributes: make URL comply to _CornucopiaStorageBackend

---
+++ Sources/CornucopiaCore/Storage/ExtendedFileAttributes.swift
@@ -0,0 +1,15 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+import Foundation
+
+extension URL: _CornucopiaCoreStorageBackend {
+
+    public func object<T: Decodable>(forKey defaultName: String) -> T? {
+        self.CC_queryExtendedAttribute(for: defaultName)
+    }
+
+    public func set<T: Encodable>(_ value: T?, forKey defaultName: String) {
+        self.CC_storeAsExtendedAttribute(item: value, for: defaultName)
+    }
+}
