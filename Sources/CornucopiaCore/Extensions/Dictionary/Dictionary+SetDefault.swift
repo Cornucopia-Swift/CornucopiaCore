157a1c7665d4c691c804003293703743b132e4e2
Dr. Michael Lauer <mickey@vanille-media.de>
2021-03-06 20:38:16 +0100

    Extensions/Dictionary: rename and document

--- Sources/CornucopiaCore/Extensions/Dictionary/Dictionary+SetDefault.swift
+++ Sources/CornucopiaCore/Extensions/Dictionary/Dictionary+SetDefault.swift
@@ -1,9 +1,10 @@
 //
 //  Cornucopia – (C) Dr. Lauer Information Technology
 //
-
 public extension Dictionary {
 
+    /// Returns the value for a given `key`, if it exists.
+    /// Otherwise, set the `defaultValue` and return it.
     mutating func CC_setDefault(_ key: Key, defaultValue: Value) -> Value {
         if let value = self[key] {
             return value
