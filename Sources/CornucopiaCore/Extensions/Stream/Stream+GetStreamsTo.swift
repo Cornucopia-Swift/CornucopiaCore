03c8d313165bf2bfe63b43fa6ad6ad7a183f23b7
Dr. Michael Lauer <mickey@vanille-media.de>
2021-03-06 12:21:05 +0100

    Extensions/Stream: unify file and host cases for more convenience

---
+++ Sources/CornucopiaCore/Extensions/Stream/Stream+GetStreamsTo.swift
@@ -0,0 +1,48 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+import Foundation
+
+public extension Stream {
+
+    enum StreamError: Error {
+        case unableToConnect
+    }
+
+    /// Returns a pair of input and output streams for accessing a service indicated by the `hostname` and `port`.
+    static func CC_getStreamsToHost(_ hostname: String, port: Int) throws -> (inputStream: InputStream, outputStream: OutputStream) {
+
+        var inputStream: InputStream?
+        var outputStream: OutputStream?
+
+        Self.getStreamsToHost(withName: hostname, port: port, inputStream: &inputStream, outputStream: &outputStream)
+        guard let istream = inputStream, let ostream = outputStream else { throw StreamError.unableToConnect }
+        return (inputStream: istream, outputStream: ostream)
+    }
+
+    /// Returns a pair of input and output streams for communicating via a file (i.e., a tty)
+    static func CC_getStreamsToFile(_ path: String, appendToOutput: Bool = false) throws -> (inputStream: InputStream, outputStream: OutputStream) {
+
+        let inputStream = InputStream.init(fileAtPath: path)
+        let outputStream = OutputStream.init(toFileAtPath: path, append: appendToOutput)
+
+        guard let istream = inputStream, let ostream = outputStream else { throw StreamError.unableToConnect }
+        return (inputStream: istream, outputStream: ostream)
+    }
+
+    /// Returns a pair of input and output streams for communicating via a URL. Supported URL schemes are `ip` and `tty`.
+    static func CC_getStreamsToURL(_ url: URL) throws -> (inputStream: InputStream, outputStream: OutputStream) {
+        precondition(url.scheme == "ip" || url.scheme == "tty", "Unsupported URL scheme \(url.scheme ?? "unknown"). Supported are 'ip' and 'tty'.")
+
+        switch url.scheme {
+
+            case "ip":
+                return try Self.CC_getStreamsToHost(url.host ?? "unknown", port: url.port ?? 0)
+            case "tty":
+                return try Self.CC_getStreamsToFile(url.path)
+
+            default:
+                fatalError()
+        }
+    }
+}
