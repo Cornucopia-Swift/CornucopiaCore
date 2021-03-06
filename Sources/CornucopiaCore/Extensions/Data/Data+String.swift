de589370d49bc763d2a31f69dfac1ff621e219cf
Dr. Michael Lauer <mickey@vanille-media.de>
2021-03-06 12:20:51 +0100

    Extensions/Data: add CC_debugString (WIP)

--- Sources/CornucopiaCore/Extensions/Data/Data+String.swift
+++ Sources/CornucopiaCore/Extensions/Data/Data+String.swift
@@ -10,4 +10,11 @@ public extension Data {
     /// _in_ the string. Hence this should rather be understood as a debugging method.
     var CC_string: String { String(data: self, encoding: .utf8) ?? "<CC_string: Can't decode data to UTF8>" }
 
+    /// The corresponding UTF8-encoded string with certain control characters escaped.
+    var CC_debugString: String {
+        guard let string = String(data: self, encoding: .utf8) else { return "<CC_debugString: Can't decode data to UTF8>" }
+        //FIXME: This is incomplete and has abysmal performance. Rewrite this to handle more/all control characters and also traversing the string only once
+        return string.replacingOccurrences(of: "\r", with: "\\r").replacingOccurrences(of: "\n", with: "\\n")
+    }
+
 }
