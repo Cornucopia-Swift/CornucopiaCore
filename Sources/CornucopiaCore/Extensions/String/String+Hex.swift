c5525a6818e9d242d3864d4bee53c1709d5d174a
Dr. Michael Lauer <mickey@vanille-media.de>
2021-03-07 15:24:44 +0100

    Extensions/String: add CC_hexDecodedUInt8

--- Sources/CornucopiaCore/Extensions/String/String+Hex.swift
+++ Sources/CornucopiaCore/Extensions/String/String+Hex.swift
@@ -23,7 +23,20 @@ public extension String {
             let index2 = Int(chars[i + 1] & 0x1F ^ 0x10)
             bytes.append(Self.map[index1] << 4 | Self.map[index2])
         }
-
         return Data(bytes)
+    }
+
+    var CC_hexDecodedUInt8: [UInt8] {
+        precondition(self.count % 2 == 0, "This works only for strings with an even number of characters")
+        let chars = Array(self.utf8)
+        var bytes = [UInt8]()
+        bytes.reserveCapacity(count / 2)
+
+        for i in stride(from: 0, to: count, by: 2) {
+            let index1 = Int(chars[i] & 0x1F ^ 0x10)
+            let index2 = Int(chars[i + 1] & 0x1F ^ 0x10)
+            bytes.append(Self.map[index1] << 4 | Self.map[index2])
+        }
+        return bytes
     }
 }
