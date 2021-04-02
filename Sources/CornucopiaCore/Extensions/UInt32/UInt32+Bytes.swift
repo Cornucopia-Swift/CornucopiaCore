96cf52c1671a37786f18b8f07c1bf12fc0ed5b1c
Dr. Michael Lauer <mickey@vanille-media.de>
2021-04-02 12:44:05 +0200

    UInt32: allow creation from UInt8 array

--- Sources/CornucopiaCore/Extensions/UInt32/UInt32+Bytes.swift
+++ Sources/CornucopiaCore/Extensions/UInt32/UInt32+Bytes.swift
@@ -3,6 +3,24 @@
 //
 public extension UInt32 {
 
+    init(_ array: [UInt8]) {
+        precondition(array.count == 4, "The source array needs to have exactly 4 octets to build an UInt32")
+        let mswHi = UInt32(array[0]) << 24
+        let mswLo = UInt32(array[1]) << 16
+        let lswHi = UInt32(array[2]) << 8
+        let lswLo = UInt32(array[3]) << 0
+        self = mswHi + mswLo + lswHi + lswLo
+    }
+
+    init(_ array: ArraySlice<UInt8>) {
+        precondition(array.count == 4, "The source array slice needs to have exactly 4 octets to build an UInt32")
+        let mswHi = UInt32(array[array.startIndex + 0]) << 24
+        let mswLo = UInt32(array[array.startIndex + 1]) << 16
+        let lswHi = UInt32(array[array.startIndex + 2]) << 8
+        let lswLo = UInt32(array[array.startIndex + 3]) << 0
+        self = mswHi + mswLo + lswHi + lswLo
+    }
+
     /// Returns the corresponding tuple of UInt8
     var CC_UInt8tuple: (mswHi: UInt8, mswLo: UInt8, lswHi: UInt8, lswLo: UInt8) {
         let mswHi: UInt8 = UInt8(self >> 24 & 0xFF)
