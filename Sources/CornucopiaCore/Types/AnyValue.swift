3dde4861c2f61a0f430788f2b22d8d7dd7b06a97
Dr. Michael Lauer <mickey@vanille-media.de>
2021-04-22 17:48:18 +0200

    AnyValue: add some convenience to get the concrete types out of AnyValue.
    This needs work, see the comment within.

--- Sources/CornucopiaCore/Types/AnyValue.swift
+++ Sources/CornucopiaCore/Types/AnyValue.swift
@@ -2,7 +2,7 @@
 //  Cornucopia – (C) Dr. Lauer Information Technology
 //
 
-// (C) Guille Gonzalez, MIT-licensed, based on https://github.com/gonzalezreal/AnyValue
+// [3RDPARTY] (C) Guille Gonzalez, MIT-licensed, based on https://github.com/gonzalezreal/AnyValue
 import Foundation
 
 public extension Cornucopia.Core {
@@ -15,8 +15,65 @@ public extension Cornucopia.Core {
         case dictionary([String: AnyValue])
         case array([AnyValue])
         case null
+
+        var anyValue: Any {
+            switch self {
+                case .string(let string): return string
+                case .bool(let bool): return bool
+                case .int(let int): return int
+                case .double(let double): return double
+                case .dictionary(let dict): return dict.CC_mappedValues { $0.anyValue }
+                case .array(let array): return array.map { $0.anyValue }
+                case .null: return NSNull()
+            }
+        }
+
+        public enum Error: Swift.Error {
+            case typeMismatch(info: String)
+            case outOfBounds(info: String)
+        }
+
+        public func value<T>() throws -> T {
+            /// FIXME: This is pretty pathetic and will lead to combinatoric explosion… is there a better way (perhaps via Reflection?) to handle this?
+            switch T.self {
+                // simple cases
+                case is String.Type:
+                    guard case let .string(string) = self else { throw Error.typeMismatch(info: "Expected \(T.self), got \(self) instead") }
+                    return string as! T
+                case is Int.Type:
+                    guard case let .int(int) = self else { throw Error.typeMismatch(info: "Expected \(T.self), got \(self) instead") }
+                    return int as! T
+                case is Int32.Type:
+                    guard case let .int(int) = self else { throw Error.typeMismatch(info: "Expected \(T.self), got \(self) instead") }
+                    return Int32(int) as! T
+                case is UInt8.Type:
+                    guard case let .int(int) = self else { throw Error.typeMismatch(info: "Expected \(T.self), got \(self) instead") }
+                    guard 0...255 ~= int else { throw Error.outOfBounds(info: "\(self) can't be represented as \(T.self)") }
+                    return UInt8(int) as! T
+                // array cases
+                case is Array<Int>.Type:
+                    guard case let .array(array) = self else { throw Error.typeMismatch(info: "Expected \(T.self), got \(self) instead") }
+                    return try (array.map { try $0.value() as Int }) as! T
+                case is Array<UInt8>.Type:
+                    guard case let .array(array) = self else { throw Error.typeMismatch(info: "Expected \(T.self), got \(self) instead") }
+                    return try (array.map { try $0.value() as UInt8 }) as! T
+                case is Array<Int32>.Type:
+                    guard case let .array(array) = self else { throw Error.typeMismatch(info: "Expected \(T.self), got \(self) instead") }
+                    return try (array.map { try $0.value() as Int32 }) as! T
+                // array of array cases
+                case is Array<Array<Int>>.Type:
+                    guard case let .array(array) = self else { throw Error.typeMismatch(info: "Expected \(T.self), got \(self) instead") }
+                    return try (array.map { try $0.value() as [Int] }) as! T
+                case is Array<Array<UInt8>>.Type:
+                    guard case let .array(array) = self else { throw Error.typeMismatch(info: "Expected \(T.self), got \(self) instead") }
+                    return try (array.map { try $0.value() as [UInt8] }) as! T
+                default:
+                    throw Error.typeMismatch(info: "No idea how to deal with \(T.self), while I'm \(self)")
+            }
+        }
     }
 
+    typealias StringAnyCollection = [String: AnyValue]
 }
 
 extension Cornucopia.Core.AnyValue: Codable {
@@ -39,8 +96,7 @@ extension Cornucopia.Core.AnyValue: Codable {
         } else if container.decodeNil() {
             self = .null
         } else {
-            let context = DecodingError.Context(codingPath: container.codingPath,
-                                                debugDescription: "Cannot decode AnyValue")
+            let context = DecodingError.Context(codingPath: container.codingPath, debugDescription: "Cannot decode AnyValue")
             throw DecodingError.typeMismatch(Cornucopia.Core.AnyValue.self, context)
         }
     }
@@ -49,20 +105,20 @@ extension Cornucopia.Core.AnyValue: Codable {
         var container = encoder.singleValueContainer()
 
         switch self {
-        case let .string(value):
-            try container.encode(value)
-        case let .int(value):
-            try container.encode(value)
-        case let .double(value):
-            try container.encode(value)
-        case let .bool(value):
-            try container.encode(value)
-        case let .dictionary(value):
-            try container.encode(value)
-        case let .array(value):
-            try container.encode(value)
-        case .null:
-            try container.encodeNil()
+            case let .string(value):
+                try container.encode(value)
+            case let .int(value):
+                try container.encode(value)
+            case let .double(value):
+                try container.encode(value)
+            case let .bool(value):
+                try container.encode(value)
+            case let .dictionary(value):
+                try container.encode(value)
+            case let .array(value):
+                try container.encode(value)
+            case .null:
+                try container.encodeNil()
         }
     }
 }
