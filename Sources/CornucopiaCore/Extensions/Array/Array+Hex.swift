dc6dbe98899179fa7e572a90b4289f95f704e84d
Dr. Michael Lauer <mickey@vanille-media.de>
2021-05-10 12:07:05 +0200

    deprecate certain conversions, these should better be handled via String Interpolation

--- Sources/CornucopiaCore/Extensions/Array/Array+Hex.swift
+++ Sources/CornucopiaCore/Extensions/Array/Array+Hex.swift
@@ -3,6 +3,10 @@
 //
 public extension Array where Element == UInt8 {
 
+    @available(*, deprecated)
     var CC_hexDecodedString: String { self.map { String(format: "%02X", $0) }.joined() }
+    @available(*, deprecated)
+    var CC_hexDecodedStringWithSpaces: String { self.map { String(format: "%02X", $0) }.joined(separator: " ") }
+
     var CC_asciiDecodedString: String { self.map { String(format: "%c", $0 > 0x08 && $0 < 0x80 ? $0 : 0x2E) }.joined() }
 }
