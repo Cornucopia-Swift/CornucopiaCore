8b8c816c9a138de7948576ee920790d2a31e5cc7
Dr. Michael Lauer <mickey@vanille-media.de>
2021-05-24 12:19:37 +0200

    Cache: protocol++

--- Sources/CornucopiaCore/Caching/Cache.swift
+++ Sources/CornucopiaCore/Caching/Cache.swift
@@ -14,6 +14,7 @@ public protocol _CornucopiaCoreUrlCache {
     typealias DataCompletionHandler = (Data?) -> ()
 
     func loadDataFor(url: URL, then: @escaping(DataCompletionHandler))
+    func loadDataFor(urlRequest: URLRequest, then: @escaping(DataCompletionHandler))
 }
 
 public extension Cornucopia.Core {
