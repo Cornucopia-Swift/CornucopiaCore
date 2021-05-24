ec6d34313146f71b054ec4c9b377e078697f9715
Dr. Michael Lauer <mickey@vanille-media.de>
2021-05-24 11:57:49 +0200

    Cache: document and make it less chatty, substituting debug w/ trace where appropriate

--- Sources/CornucopiaCore/Caching/Cache.swift
+++ Sources/CornucopiaCore/Caching/Cache.swift
@@ -14,14 +14,13 @@ public protocol _CornucopiaCoreUrlCache {
     typealias DataCompletionHandler = (Data?) -> ()
 
     func loadDataFor(url: URL, then: @escaping(DataCompletionHandler))
-
 }
 
 public extension Cornucopia.Core {
 
     typealias UrlCache = _CornucopiaCoreUrlCache
 
-    /// A simple cache for data gathered using a HTTP GET call
+    /// A simple cache for data gathered using a HTTP(s) GET call
     class Cache: UrlCache {
 
         var memoryCache = ThreadSafeDictionary<String, Data>()
@@ -29,41 +28,42 @@ public extension Cornucopia.Core {
         let path: String
         let urlSession: URLSession = URLSession(configuration: URLSessionConfiguration.ephemeral)
 
+        /// Loads the data for the specified `URL` and calls the completion handler with the data unless all cache levels fail.
+        /// NOTE: The completion handler will be called in a background thread context.
         public func loadDataFor(url: URL, then: @escaping (DataCompletionHandler)) {
             let urlRequest = URLRequest(url: url)
             self.loadDataFor(urlRequest: urlRequest, then: then)
         }
 
         /// Loads the data for the specified `URLRequest` and calls the completion handler with the data unless all cache levels fail.
-        /// The completion handler will be called in a background thread context.
+        /// NOTE: The completion handler will be called in a background thread context.
         public func loadDataFor(urlRequest: URLRequest, then: @escaping (DataCompletionHandler)) {
-
-            print("urlRequest: \(urlRequest)")
-
+            // We're dispatching into a seperate parallel queue to not block the main queue with disk operations
+            //FIXME: I'm almost sure that this is not fully concurrency safe. It is due for a rewrite when Swift 5.5 `actor`s are available
             DispatchQueue.global().async {
 
                 let url = urlRequest.url!
                 let md5 = url.absoluteString.CC_md5
                 if let data = self.memoryCache[md5] {
-                    logger.debug("Memory HIT for \(url)")
+                    logger.trace("Memory HIT for \(url)")
                     then(data)
                     return
                 }
-                logger.debug("Memory MISS \(url)")
+                logger.trace("Memory MISS \(url)")
                 let pathInCache = self.path + "/\(md5)"
                 let urlInCache = URL(fileURLWithPath: pathInCache)
                 if FileManager.default.fileExists(atPath: pathInCache) {
-                    logger.debug("Disk HIT for \(url)")
+                    logger.trace("Disk HIT for \(url)")
                     do {
                         let data = try Data(contentsOf: urlInCache, options: .alwaysMapped)
                         then(data)
                         self.memoryCache[md5] = data
                         return
                     } catch {
-                        logger.error("Can't load \(pathInCache): \(error)")
+                        logger.debug("Can't load \(pathInCache): \(error)")
                     }
                 }
-                logger.debug("Disk MISS for \(url)")
+                logger.trace("Disk MISS for \(url)")
                 let task = self.urlSession.dataTask(with: urlRequest) { data, urlResponse, error in
                     guard error == nil else {
                         logger.notice("Network MISS for \(url): \(error!)")
@@ -81,7 +81,7 @@ public extension Cornucopia.Core {
                         then(nil)
                         return
                     }
-                    logger.debug("Network HIT for \(url)")
+                    logger.trace("Network HIT for \(url)")
                     then(data)
                     do {
                         try data.write(to: urlInCache, options: .atomic)
@@ -99,12 +99,10 @@ public extension Cornucopia.Core {
             self.path = FileManager.CC_pathInCachesDirectory(suffix: "Cornucopia.Core.Cache/\(name)/")
             do {
                 try FileManager.default.createDirectory(atPath: self.path, withIntermediateDirectories: true)
-                logger.info("Created directory for cache \(self.name) at \(self.path)")
+                logger.debug("Using cache directory \(self.path)")
             } catch {
-                logger.error("Can't create directory \(self.path): \(error)")
+                logger.error("Can't create cache directory \(self.path): \(error)")
             }
         }
-
     }
-
 }
