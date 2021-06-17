53ff597482bcf04660bed463fa091c24733c51e4
Dr. Michael Lauer <mickey@vanille-media.de>
2021-06-17 15:14:37 +0200

    String+ContentValidation: add linux-support
    work around swift-corelibs-foundation not fully implementing the NSPredicate API

--- Sources/CornucopiaCore/Extensions/String/String+ContentValidation.swift
+++ Sources/CornucopiaCore/Extensions/String/String+ContentValidation.swift
@@ -5,11 +5,15 @@ import Foundation
 
 public extension String {
 
-    private static var PhoneRegex: String = "^((\\+)|(00))[0-9]{6,14}|[0-9]{6,14}$"
+    private static var PhoneRegexSource: String = "^((\\+)|(00))[0-9]{6,14}|[0-9]{6,14}$"
+    private static var PhoneRegex: NSRegularExpression = try! .init(pattern: Self.PhoneRegexSource, options: [])
 
     var CC_isValidPhoneNumber: Bool {
-
+        #if canImport(ObjectiveC)
         let predicate = NSPredicate(format: "SELF MATCHES %@", Self.PhoneRegex)
         return predicate.evaluate(with: self)
+        #else
+        return Self.PhoneRegex.numberOfMatches(in: self, range: self.CC_nsRange) > 0
+        #endif
     }
 }
