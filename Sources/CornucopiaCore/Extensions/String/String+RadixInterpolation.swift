df99e7e76b4e75730fca0333179912a89a491c0c
Dr. Michael Lauer <mickey@vanille-media.de>
2021-07-15 18:03:05 +0200

    String+RadixInterpolation: docs++

--- Sources/CornucopiaCore/Extensions/String/String+RadixInterpolation.swift
+++ Sources/CornucopiaCore/Extensions/String/String+RadixInterpolation.swift
@@ -4,17 +4,20 @@
 //  Inspired by Erica Sadun, https://ericasadun.com/2018/12/14/more-fun-with-swift-5-string-interpolation-radix-formatting/
 public extension String.StringInterpolation {
 
+    /// The desired base. Use it, like e.g. "\(0xdeadbeef, radix: .hex, .prefix: true, .width = 4)"
     enum Radix: Int {
         case binary = 2
         case octal = 8
         case decimal = 10
         case hex = 16
 
+        /// The base's prefix.
         var prefix: String {
             return [.binary: "0b", .octal: "0o", .hex: "0x"][self, default: ""]
         }
     }
 
+    /// Formatting a `BinaryInteger`.
     mutating func appendInterpolation<I: BinaryInteger>(_ value: I, radix: Radix, prefix: Bool = false, toWidth width: Int = 0) {
 
         var string = String(value, radix: radix.rawValue).uppercased()
@@ -26,7 +29,8 @@ public extension String.StringInterpolation {
         }
         appendInterpolation(string)
     }
-
+    
+    /// Formatting a `[BinaryInteger]`.
     mutating func appendInterpolation<I: BinaryInteger>(_ array: [I], radix: Radix, prefix: Bool = false, toWidth width: Int = 0, separator: String = "") {
 
         for (idx, element) in array.enumerated() {
