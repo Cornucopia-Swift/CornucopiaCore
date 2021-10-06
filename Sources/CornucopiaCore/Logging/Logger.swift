cc3556b09f763b1f8f4f3cd9375cb3ec8e5676b3
Dr. Michael Lauer <mickey@vanille-media.de>
2021-10-06 13:10:37 +0200

    Logger: Get rid of using the os_log infrastructure on Apple platforms
    This might not be the most efficient one. Although os_log (or the more modern os.Logger) is preferred on Apple platforms,
    Xcode spoils this with its incredible amount of spam printed by the iOS simulator.
    Setting OS_ACTIVITY_MODE=disable not only mutes the spam, but _all_ of the os_log (and os.Logger) output.
    Eventually this will gain another environment variable for specifying the log message target (stderr, udp, tcp, etc.)

--- Sources/CornucopiaCore/Logging/Logger.swift
+++ Sources/CornucopiaCore/Logging/Logger.swift
@@ -3,20 +3,22 @@
 //
 import Foundation
 
-#if canImport(os)
-import os
-#endif
-
 public extension Cornucopia.Core {
 
     /// A Logger.
-    /// Yes, I know, on Apple platforms this is not so efficient, since OSLog (and Logger) is not supposed to be wrapped.
-    /// Still, this code also applies to non-Apple platforms, hence there is no choice other than sprinkling the call-site
-    /// with loads of #if/#else/#endif – which is non-negotiable in my opinion.
+    /// This might not be the most efficient one. Although os_log (or the more modern os.Logger) is preferred on Apple platforms,
+    /// Xcode spoils this with its incredible amount of spam. Setting OS_ACTIVITY_MODE=disable not only mutes the spam, but _all_
+    /// of the os_log (and os.Logger) output.
+    /// Eventually this will gain another environment variable for specifying the log message target (stderr, udp, tcp, etc.)
     struct Logger {
 
         public static let includeDebug: Bool = ProcessInfo.processInfo.environment["LOGLEVEL"] == "DEBUG" || Self.includeTrace
         public static let includeTrace: Bool = ProcessInfo.processInfo.environment["LOGLEVEL"] == "TRACE"
+        public static let timeFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+            return formatter
+        }()
 
         /// The log level.
         public enum Level: String {
@@ -26,93 +28,79 @@ public extension Cornucopia.Core {
             case notice
             case error
             case fault
-#if !canImport(os)
             case `default`
-#endif
 
-#if canImport(os)
-            public var osLogType: OSLogType {
+            public var character: String {
                 switch self {
-                    case .trace:  return .debug
-                    case .debug:  return .debug
-                    case .info:   return .info
-                    case .notice: return .default
-                    case .error:  return .error
-                    case .fault:  return .fault
+                    case .trace:   return "T"
+                    case .debug:   return "D"
+                    case .info:    return "I"
+                    case .notice:  return "N"
+                    case .error:   return "E"
+                    case .fault:   return "F"
+                    case .default: return "?"
                 }
             }
-#else
-            public var osLogType: Level { self }
-#endif
         }
 
-#if canImport(os)
-        public var oslog: OSLog
-#else
-        public var oslog: Bool = false // dummy
         public let subsystem: String
         public let category: String
-#endif
 
         /// Create the logger with the given `subsystem` and `category`.
         public init(subsystem: String = Bundle.main.bundleIdentifier ?? "unknown.bundle.identifier", category: String = #fileID) {
-#if canImport(os)
-            var category = category.hasSuffix(".swift") ? category.split(separator: "/").last!.replacingOccurrences(of: ".swift", with: "") : category
-            oslog = OSLog(subsystem: subsystem, category: category)
-#else
+            let category = category.hasSuffix(".swift") ? category.split(separator: "/").last!.replacingOccurrences(of: ".swift", with: "") : category
             self.subsystem = subsystem
             self.category = category
-#endif
         }
 
-#if !canImport(os)
         @_transparent
-        public func os_log(_ format: String, log: Bool, type: Level, _ message: String) {
-            print("[\(self.subsystem):\(self.category)] (\(type)) \(message)")
+        public func log(_ message: String, level: Level) {
+            //NOTE: On Apple platforms, we use a private API to gather the thread number, on Linux we can only show 1 (main) or 0 (secondary).
+            #if canImport(ObjectiveC)
+            let threadNumber = (Thread.current.value(forKeyPath: "private.seqNum") as? NSNumber)?.intValue ?? 0
+            #else
+            let threadNumber = Thread.current.isMainThread ? 1 : 0
+            #endif
+            let timestamp = Self.timeFormatter.string(from: Date())
+            print("\(timestamp) [\(self.subsystem):\(self.category)] <\(threadNumber)> (\(level.character)) \(message)")
         }
-#endif
 
-        /// Log a trace message. Trace messages are only processed, if the preprocessor symbols DEBUG and TRACE are set.
+        /// Log a trace message. Trace messages are only processed, if the environment variable LOGLEVEL is TRACE
         @inlinable
         public func trace(_ message: @autoclosure ()->String ) {
             guard Self.includeTrace else { return }
-            os_log("%s", log: oslog, type: .debug, message())
+            log(message(), level: .trace)
         }
 
-        /// Log a debug message.
+        /// Log a debug message. Debug messages are only processed, if the environment variable LOGLEVEL is DEBUG or TRACE
         @inlinable
-        public func debug(_ message: @autoclosure () -> String) {
+        public func debug(_ message: @autoclosure ()->String) {
             guard Self.includeDebug else { return }
-            os_log("%s", log: oslog, type: .debug, message())
+            log(message(), level: .debug)
         }
 
         /// Log an info message.
         @inlinable
         public func info(_ message: String) {
-            os_log("%s", log: oslog, type: .info, message)
+            log(message, level: .info)
         }
 
         /// Log a notice (warning) message.
         @inlinable
         public func notice(_ message: String) {
-            os_log("%s", log: oslog, type: .default, message)
+            log(message, level: .default)
         }
 
         /// Log an error message.
         @inlinable
         public func error(_ message: String) {
-            os_log("%s", log: oslog, type: .error, message)
+            log(message, level: .error)
         }
 
         /// Log a fault message.
         @inlinable
         public func fault(_ message: String) {
-            os_log("%s", log: oslog, type: .fault, message)
-        }
-
-        /// Log a message with the specified log level.
-        @inlinable func log(_ message: String, level: Level) {
-            os_log("%s", log: oslog, type: level.osLogType, message)
+            log(message, level: .fault)
         }
     }
 }
