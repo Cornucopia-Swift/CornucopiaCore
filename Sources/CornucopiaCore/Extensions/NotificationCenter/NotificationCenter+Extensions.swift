4f8d47110a4a1731cad3cf0a607f4557e6558541
Dr. Michael Lauer <mickey@vanille-media.de>
2021-11-15 09:47:09 +0100

    Extensions: Override the KeyedDecodingContainer's and KeyedEncodingContainer's default behavior for missing keys.
    This is necessary to get codable property wrappers wrapping optional types working.
    See https://forums.swift.org/t/using-property-wrappers-with-codable for the gory details.

--- Sources/CornucopiaCore/Extensions/NotificationCenter/NotificationCenter+Extensions.swift
+++ Sources/CornucopiaCore/Extensions/NotificationCenter/NotificationCenter+Extensions.swift
@@ -8,5 +8,4 @@ public extension NotificationCenter {
     func CC_postAsync(on queue: DispatchQueue, name: Notification.Name, object: Any? = nil) {
         queue.async { self.post(name: name, object: object) }
     }
-
 }
