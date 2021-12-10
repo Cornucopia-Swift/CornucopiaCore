df66410c3749590d966971d1098e81f6d3af0b59
Dr. Michael Lauer <mickey@vanille-media.de>
2021-12-10 09:50:24 +0100

    Add a portable way to gather the current thread number.

---
+++ Sources/CornucopiaCore/Extensions/Thread/Thread+Number.swift
@@ -0,0 +1,28 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+import Foundation
+
+extension Thread {
+
+    private static var lock: NSLock = .init()
+    private static var threadNumber: Int = 1
+    private static let key: String = "dev.cornucopia.core.thread-number"
+
+    /// Returns an associated number for the current thread.
+    /// NOTE: This number gets assigned the first time it is being queried, hence the order
+    /// does not necessarily reflect the thread creation order. For the main thread though,
+    /// this property always returns `0`.
+    public var CC_number: Int {
+
+        Self.lock.lock()
+        defer { Self.lock.unlock() }
+        guard !Thread.current.isMainThread else { return 0 }
+        guard let threadNumber = Thread.current.threadDictionary.object(forKey: Self.key) else {
+            Thread.current.threadDictionary.setObject(Self.threadNumber, forKey: Self.key)
+            defer { Self.threadNumber += 1 }
+            return Self.threadNumber
+        }
+        return threadNumber as! Int
+    }
+}
