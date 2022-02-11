f08ee80b07383ae243eed0f35e156ff97dc5627c
Dr. Michael Lauer <mickey@vanille-media.de>
2022-02-11 21:04:20 +0100

    Extensions: Add Sequence+ASCII

---
+++ Sources/CornucopiaCore/Extensions/Sequence/Sequence+ASCII.swift
@@ -0,0 +1,9 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+public extension Sequence where Element == UInt8 {
+
+    /// Returns an ASCII decoded string.
+    //FIXME: Shouldn't this rather be solved via a (customizable) string interpolation?
+    var CC_asciiDecodedString: String { self.map { String(format: "%c", $0 > 0x2F && $0 < 0x80 ? $0 : 0x2E) }.joined() }
+}
