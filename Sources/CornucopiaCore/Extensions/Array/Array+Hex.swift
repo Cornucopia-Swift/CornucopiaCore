416e7773a55a1edd339f443e8cbc3ea9e0494cbe
Dr. Michael Lauer <mickey@vanille-media.de>
2022-02-12 12:29:13 +0100

    Extensions: Remove Array's CC_asciiDecodedString in favor of the more generic extension on Sequence.

--- Sources/CornucopiaCore/Extensions/Array/Array+Hex.swift
+++ Sources/CornucopiaCore/Extensions/Array/Array+Hex.swift
@@ -7,6 +7,4 @@ public extension Array where Element == UInt8 {
     var CC_hexDecodedString: String { self.map { String(format: "%02X", $0) }.joined() }
     @available(*, deprecated)
     var CC_hexDecodedStringWithSpaces: String { self.map { String(format: "%02X", $0) }.joined(separator: " ") }
-
-    var CC_asciiDecodedString: String { self.map { String(format: "%c", $0 > 0x08 && $0 < 0x80 ? $0 : 0x2E) }.joined() }
 }
