b2e9c42e4faf1fc5a43aee443ddf5b011a6d8edc
Dr. Michael Lauer <mickey@vanille-media.de>
2022-03-03 21:36:43 +0100

    Extensions: add String+PointerInterpolation.
    This introduces a modern replacement for the string format specifier %p.

---
+++ Sources/CornucopiaCore/Extensions/StringInterpolation/String+PointerInterpolation.swift
@@ -0,0 +1,13 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+extension String.StringInterpolation {
+
+    @inlinable mutating func appendInterpolation<OBJ: AnyObject>(_ value: OBJ, pointerWithPrefix: Bool) {
+
+        let prefix: String = pointerWithPrefix ? "0x" : ""
+        let string: String = String(unsafeBitCast(value, to: Int.self), radix: 16, uppercase: true)
+        appendInterpolation("\(prefix)\(string)")
+    }
+}
+
