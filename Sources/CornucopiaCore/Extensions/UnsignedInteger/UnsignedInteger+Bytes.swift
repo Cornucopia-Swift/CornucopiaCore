4ac8eacc8d78b2e4a1d5655cd2f7a52578873f06
Dr. Michael Lauer <mickey@vanille-media.de>
2022-03-03 20:40:40 +0100

    Extensions: add UnsignedInteger+Bytes, converting a Collection<UInt8> to UnsignedIntegers

---
+++ Sources/CornucopiaCore/Extensions/UnsignedInteger/UnsignedInteger+Bytes.swift
@@ -0,0 +1,18 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+public extension UnsignedInteger {
+
+    init<T: Collection>(fromUInt8Collection bytes: T) where T.Element == UInt8 {
+        precondition(bytes.count <= MemoryLayout<Self>.size)
+
+        var value: Self = 0
+
+        for byte in bytes {
+            value <<= 8
+            value |= Self(byte)
+        }
+
+        self.init(value)
+    }
+}
---
+++ Tests/CornucopiaCoreTests/Extensions/UnsignedInteger/UnsignedIntegerExtensions.swift
@@ -0,0 +1,58 @@
+import XCTest
+
+import CornucopiaCore
+
+class UnsignedIntegerExtensions: XCTestCase {
+
+    func testUInt8() {
+
+        let given: [UInt8] = [0xAA]
+        let when = UInt8(fromUInt8Collection: given)
+        XCTAssertEqual(when, 0xAA)
+    }
+
+    func testUInt16() {
+
+        let given: [UInt8] = [0xAA]
+        let when = UInt16(fromUInt8Collection: given)
+        XCTAssertEqual(when, 0xAA)
+
+        let given2: [UInt8] = [0xAA, 0xBB]
+        let when2 = UInt16(fromUInt8Collection: given2)
+        XCTAssertEqual(when2, 0xAABB)
+    }
+
+    func testUInt32() {
+
+        let given: [UInt8] = [0xAA]
+        let when = UInt32(fromUInt8Collection: given)
+        XCTAssertEqual(when, 0xAA)
+
+        let given2: [UInt8] = [0xAA, 0xBB]
+        let when2 = UInt32(fromUInt8Collection: given2)
+        XCTAssertEqual(when2, 0xAABB)
+
+        let given3: [UInt8] = [0xAA, 0xBB, 0xCC]
+        let when3 = UInt32(fromUInt8Collection: given3)
+        XCTAssertEqual(when3, 0xAABBCC)
+    }
+
+    func testUInt64() {
+
+        let given: [UInt8] = [0xAA]
+        let when = UInt64(fromUInt8Collection: given)
+        XCTAssertEqual(when, 0xAA)
+
+        let given2: [UInt8] = [0xAA, 0xBB]
+        let when2 = UInt64(fromUInt8Collection: given2)
+        XCTAssertEqual(when2, 0xAABB)
+
+        let given3: [UInt8] = [0xAA, 0xBB, 0xCC]
+        let when3 = UInt64(fromUInt8Collection: given3)
+        XCTAssertEqual(when3, 0xAABBCC)
+
+        let given4: [UInt8] = [0xAA, 0xBB, 0xCC, 0xDD]
+        let when4 = UInt64(fromUInt8Collection: given4)
+        XCTAssertEqual(when4, 0xAABBCCDD)
+    }
+}
