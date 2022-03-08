597d514df95f5957bd835f822e3c38bc2a8d83ed
Dr. Michael Lauer <mickey@vanille-media.de>
2022-03-08 16:17:09 +0100

    Features: add convenient concurrency helper asyncWithTimeout

---
+++ Sources/CornucopiaCore/Features/Concurrency/AsyncWithTimeout.swift
@@ -0,0 +1,38 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+import Foundation
+
+#if compiler(>=5.5) && canImport(_Concurrency)
+extension Date: @unchecked Sendable {}
+
+@available(iOS 15, tvOS 15, watchOS 8, macOS 12, *)
+extension Cornucopia.Core {
+
+    public struct AsyncWithTimeoutError: Error, Equatable {}
+
+    public func asyncWithTimeout<ResultType: Sendable>(seconds: TimeInterval, body: @escaping @Sendable () async throws -> ResultType) async throws -> ResultType {
+
+        try await withThrowingTaskGroup(of: ResultType.self) { group in
+
+            let deadline = Date(timeIntervalSinceNow: seconds)
+
+            // add actual work
+            group.addTask { try await body() }
+
+            // add timeout
+            group.addTask {
+                let interval = deadline.timeIntervalSinceNow
+                if interval > 0 {
+                    try await Task.CC_sleep(seconds: interval)
+                }
+                try Task.checkCancellation()
+                throw AsyncWithTimeoutError()
+            }
+            let result = try await group.next()!
+            group.cancelAll()
+            return result
+        }
+    }
+}
+#endif
