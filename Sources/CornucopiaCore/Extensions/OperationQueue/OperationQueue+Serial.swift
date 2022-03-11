cbe1c97cac7ce2acd2e5dfe1a746ad75454f68b8
Dr. Michael Lauer <mickey@vanille-media.de>
2022-03-11 13:40:02 +0100

    Extensions: OperationQueue+Serial: fix compiling on Linux

--- Sources/CornucopiaCore/Extensions/OperationQueue/OperationQueue+Serial.swift
+++ Sources/CornucopiaCore/Extensions/OperationQueue/OperationQueue+Serial.swift
@@ -5,8 +5,12 @@ import Foundation
 
 public extension OperationQueue {
 
-    static func CC_serial(name: String? = nil) -> Self {
+    static func CC_serial(name: String? = nil) -> OperationQueue {
+#if canImport(ObjectiveC)
         let opq = Self()
+#else
+        let opq = OperationQueue()
+#endif
         opq.maxConcurrentOperationCount = 1
         opq.name = name
         return opq
