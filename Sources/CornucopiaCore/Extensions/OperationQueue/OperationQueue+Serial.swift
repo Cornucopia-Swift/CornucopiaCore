1a90cdbb9040f569398435b2c4ee55ea02fda3b3
Dr. Michael Lauer <mickey@vanille-media.de>
2022-04-12 17:58:31 +0200

    Extensions: OperationQueue+Serial: add QoS parameter

--- Sources/CornucopiaCore/Extensions/OperationQueue/OperationQueue+Serial.swift
+++ Sources/CornucopiaCore/Extensions/OperationQueue/OperationQueue+Serial.swift
@@ -5,13 +5,14 @@ import Foundation
 
 public extension OperationQueue {
 
-    static func CC_serial(name: String? = nil) -> OperationQueue {
+    static func CC_serial(name: String? = nil, qos: QualityOfService = .default) -> OperationQueue {
 #if canImport(ObjectiveC)
         let opq = Self()
 #else
         let opq = OperationQueue()
 #endif
         opq.maxConcurrentOperationCount = 1
+        opq.qualityOfService = qos
         opq.name = name
         return opq
     }
