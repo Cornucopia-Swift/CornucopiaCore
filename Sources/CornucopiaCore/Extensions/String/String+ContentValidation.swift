166a30ed7c9663e49dd8768f0e5f7eb8bb7fce9d
Dr. Michael Lauer <mickey@vanille-media.de>
2022-04-26 15:02:22 +0200

    String+ContentValidation: NSDataDetector is only available on Apple platforms.

--- Sources/CornucopiaCore/Extensions/String/String+ContentValidation.swift
+++ Sources/CornucopiaCore/Extensions/String/String+ContentValidation.swift
@@ -13,11 +13,20 @@ public extension String {
     }
 
     var CC_isValidEmailAddress: Bool {
+        #if canImport(ObjectiveC)
         let link = "mailto:" + self
         guard let emailDetector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue) else { return false }
         let matches = emailDetector.matches(in: link, options: NSRegularExpression.MatchingOptions.anchored, range: NSRange(location: 0, length: link.count))
         guard matches.count == 1 else { return false }
         return matches[0].url?.scheme == "mailto"
+        #else
+        guard self.count > 3 else { return false }
+        guard self.characters.filter { $0 == "@" }.count == 1 else { return false }
+        guard self.characters.filter { $0 == "." }.count == 1 else { return false }
+        let components = self.split(".")
+        guard components[1].count > 1 else { return false }
+        return true
+        #endif
     }
 }
 
