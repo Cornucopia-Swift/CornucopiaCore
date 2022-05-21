bacd554023f5d3868513c7a3fc10a0b92b4a8976
Dr. Michael Lauer <mickey@vanille-media.de>
2022-05-21 14:36:42 +0200

    Stream: Stream+GetStreamsTo: Add convenience method for getting a connected stream pair bound to a specific network interface.
    I have witnessed the default `getStreamsToHost(withName hostname: String, port: Int, inputStream: AutoreleasingUnsafeMutablePointer<InputStream?>?, outputStream:
    AutoreleasingUnsafeMutablePointer<OutputStream?>?)` picking up the wrong interface in some cases, in particular, when
    a link local interface has just come up while your app was running.

--- Sources/CornucopiaCore/Extensions/Stream/Stream+GetStreamsTo.swift
+++ Sources/CornucopiaCore/Extensions/Stream/Stream+GetStreamsTo.swift
@@ -7,6 +7,7 @@ import Foundation
 public extension Stream {
 
     enum StreamError: Error {
+        case connectionFailed(details: NSError)
         case unableToConnect
         case notImplemented
     }
@@ -50,6 +51,40 @@ public extension Stream {
                 fatalError()
         }
     }
+
+    static func CC_getStreamsToHost(_ hostname: String, port: Int, via interface: String) throws -> (inputStream: InputStream, outputStream: OutputStream) {
+
+        var sockaddress = sockaddr_in()
+        let sockaddress_size = socklen_t(MemoryLayout<sockaddr_in>.size)
+
+        guard inet_pton(PF_INET, hostname.cString(using: .utf8), &sockaddress.sin_addr) != -1 else { throw StreamError.connectionFailed(details: errno.CC_posixError) }
+        sockaddress.sin_port = UInt16(port).bigEndian
+        sockaddress.sin_family = sa_family_t(AF_INET)
+        // Create native socket handle, if sockhandle < 0, error occurred, (check errno)
+        let sockHandle = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)
+        guard sockHandle >= 0 else { throw StreamError.connectionFailed(details: errno.CC_posixError) }
+
+        var interfaceIndex = if_nametoindex(interface.cString(using: .utf8))
+        guard setsockopt(sockHandle, IPPROTO_IP, IP_BOUND_IF, &interfaceIndex, socklen_t(MemoryLayout<UInt32>.size)) >= 0 else { throw StreamError.connectionFailed(details: errno.CC_posixError) }
+
+        let connectResult = withUnsafePointer(to: &sockaddress) { pointer in
+            return pointer.withMemoryRebound(to: sockaddr.self, capacity: Int(sockaddress_size)) { pointer in
+                return connect(sockHandle, pointer, sockaddress_size)
+            }
+        }
+        guard connectResult >= 0 || errno == EINPROGRESS else { throw StreamError.connectionFailed(details: errno.CC_posixError) }
+
+        var readStream: Unmanaged<CFReadStream>?
+        var writeStream: Unmanaged<CFWriteStream>?
+        CFStreamCreatePairWithSocket(kCFAllocatorDefault, sockHandle, &readStream, &writeStream)
+
+        let istream = readStream!.takeRetainedValue() as InputStream
+        let ostream = writeStream!.takeRetainedValue() as OutputStream
+
+        istream.setProperty(kCFBooleanTrue, forKey: Stream.PropertyKey(kCFStreamPropertyShouldCloseNativeSocket as String))
+        ostream.setProperty(kCFBooleanTrue, forKey: Stream.PropertyKey(kCFStreamPropertyShouldCloseNativeSocket as String))
+
+        return (istream, ostream)
+    }
 }
 #endif
-
