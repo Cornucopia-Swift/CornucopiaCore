3038c9fb9031ea5bc555a11e91334ce47352c1b4
Dr. Michael Lauer <mickey@vanille-media.de>
2022-10-18 12:08:53 +0200

    MemoryAddressOf: No need to put it into a namespace.

--- Sources/CornucopiaCore/Features/LowLevel/MemoryAddressOf.swift
+++ Sources/CornucopiaCore/Features/LowLevel/MemoryAddressOf.swift
@@ -1,8 +1,6 @@
 //
 //  Cornucopia – (C) Dr. Lauer Information Technology
 //
-public extension Cornucopia.Core {
 
-    /// Returns the memory address of an object. NOTE: Unsafe, works only where the sizeof(ptr) == sizeof(Int).
-    static func CC_memoryAddressOf<T: AnyObject>(_ object: T) -> Int { unsafeBitCast(object, to: Int.self) }
-}
+/// Returns the memory address of an object. NOTE: Unsafe, works only where the sizeof(ptr) == sizeof(Int).
+func CC_memoryAddressOf<T: AnyObject>(_ object: T) -> Int { unsafeBitCast(object, to: Int.self) }
