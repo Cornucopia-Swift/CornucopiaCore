3cd4537ec01eae64cdd6a9d822419e9ef401072c
Dr. Michael Lauer <mickey@vanille-media.de>
2023-01-19 13:59:59 +0100

    Let FileHandle conform to TextOutputStream. That way it can be a print() destination.

---
+++ Sources/CornucopiaCore/Extensions/FileHandle/FileHandle+TextOutputStream.swift
@@ -0,0 +1,12 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+import Foundation
+
+extension FileHandle: TextOutputStream {
+
+    public func write(_ string: String) {
+        let data = Data(string.utf8)
+        self.write(data)
+    }
+}
