319f468f576806f79b17ca8cc903bdaa61406c20
Dr. Michael Lauer <mickey@vanille-media.de>
2023-03-07 17:24:13 +0100

    Data: introduce array conversions

--- Sources/CornucopiaCore/Extensions/Data/Data+ArrayConversion.swift
+++ Sources/CornucopiaCore/Extensions/Data/Data+ArrayConversion.swift
@@ -5,11 +5,11 @@ import Foundation
 
 extension Data {
 
-    public static func CC_fromArray<T>(values: [T]) -> Self {
+    public static func CC_fromArray<T>(_ values: [T]) -> Self {
         Self.init(values.withUnsafeBytes { Data($0) })
     }
 
-    func CC_toArray<T>(type: T.Type) -> [T] where T: ExpressibleByIntegerLiteral {
+    public func CC_toArray<T>(of: T.Type) -> [T] where T: ExpressibleByIntegerLiteral {
         var array = Array<T>(repeating: 0, count: self.count/MemoryLayout<T>.stride)
         _ = array.withUnsafeMutableBytes { copyBytes(to: $0) }
         return array
