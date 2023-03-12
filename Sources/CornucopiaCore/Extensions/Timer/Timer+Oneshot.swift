e99c4274b3cc5d639085463554d0540d8ba80f90
Dr. Michael Lauer <mickey@vanille-media.de>
2023-03-12 13:18:34 +0100

    Timer: Add Timer+Oneshot, a convenience creation method for RunLoop-based OneShot timers.

---
+++ Sources/CornucopiaCore/Extensions/Timer/Timer+Oneshot.swift
@@ -0,0 +1,17 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+import Dispatch
+import Foundation
+
+extension Timer {
+
+    /// Returns a ``Timer``, scheduled to fire exactly once after the specified `interval`.
+    /// If you're not supplying a ``RunLoop`` argument, we're using the one for the current ``Thread``.
+    public static func CC_oneShot(interval: DispatchTimeInterval, runloop: RunLoop = .current, mode: RunLoop.Mode = .common, block: @escaping @Sendable (Timer) -> Void) -> Timer {
+
+        let timer = Timer(fire: Date() + interval.CC_timeInterval, interval: 0, repeats: false, block: block)
+        runloop.add(timer, forMode: mode)
+        return timer
+    }
+}
