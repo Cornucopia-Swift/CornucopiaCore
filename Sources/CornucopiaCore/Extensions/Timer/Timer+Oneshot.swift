d924738aa3ac42acc1ffa5df134b17471fa5e321
Dr. Michael Lauer <mickey@vanille-media.de>
2023-03-22 17:23:33 +0100

    Timer+Oneshot: Return nil in the case of a timer with interval .never.
    NOTE: This is an API change, sorry.

--- Sources/CornucopiaCore/Extensions/Timer/Timer+Oneshot.swift
+++ Sources/CornucopiaCore/Extensions/Timer/Timer+Oneshot.swift
@@ -8,8 +8,8 @@ extension Timer {
 
     /// Returns a ``Timer``, scheduled to fire exactly once after the specified `interval`.
     /// If you're not supplying a ``RunLoop`` argument, we're using the one for the current ``Thread``.
-    public static func CC_oneShot(interval: DispatchTimeInterval, runloop: RunLoop = .current, mode: RunLoop.Mode = .common, block: @escaping @Sendable (Timer) -> Void) -> Timer {
-
+    public static func CC_oneShot(interval: DispatchTimeInterval, runloop: RunLoop = .current, mode: RunLoop.Mode = .common, block: @escaping @Sendable (Timer) -> Void) -> Timer? {
+        guard interval != .never else { return nil }
         let timer = Timer(fire: Date() + interval.CC_timeInterval, interval: 0, repeats: false, block: block)
         runloop.add(timer, forMode: mode)
         return timer
