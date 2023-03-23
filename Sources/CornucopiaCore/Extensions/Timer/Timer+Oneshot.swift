a246f3d30b0a01ec4ee79f8f3e5771928f596055
Dr. Michael Lauer <mickey@vanille-media.de>
2023-03-23 07:57:54 +0000

    Timer+Oneshot: Repair Linux build.
    Yet another Foundation API mismatch: https://github.com/apple/swift-corelibs-foundation/issues/4724

--- Sources/CornucopiaCore/Extensions/Timer/Timer+Oneshot.swift
+++ Sources/CornucopiaCore/Extensions/Timer/Timer+Oneshot.swift
@@ -9,9 +9,14 @@ extension Timer {
     /// Returns a ``Timer``, scheduled to fire exactly once after the specified `interval`.
     /// If you're not supplying a ``RunLoop`` argument, we're using the one for the current ``Thread``.
     public static func CC_oneShot(interval: DispatchTimeInterval, runloop: RunLoop = .current, mode: RunLoop.Mode = .common, block: @escaping @Sendable (Timer) -> Void) -> Timer? {
-        guard interval != .never else { return nil }
-        let timer = Timer(fire: Date() + interval.CC_timeInterval, interval: 0, repeats: false, block: block)
-        runloop.add(timer, forMode: mode)
-        return timer
+        //This does not work on Linux due to https://github.com/apple/swift-corelibs-foundation/issues/4724
+        //guard interval != .never else { return nil }
+        switch interval {
+            case .never: return nil
+            default:
+               let timer = Timer(fire: Date() + interval.CC_timeInterval, interval: 0, repeats: false, block: block)
+               runloop.add(timer, forMode: mode)
+               return timer
+        }
     }
 }
