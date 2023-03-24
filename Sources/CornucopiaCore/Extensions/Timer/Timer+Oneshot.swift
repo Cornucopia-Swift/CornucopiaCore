7c9a4f29cc1d651765f103b1732c3d4e07c60e8e
Dr. Michael Lauer <mickey@vanille-media.de>
2023-03-24 12:29:15 +0100

    Timer+Oneshot: Provide an additional constructor that takes a Date.

--- Sources/CornucopiaCore/Extensions/Timer/Timer+Oneshot.swift
+++ Sources/CornucopiaCore/Extensions/Timer/Timer+Oneshot.swift
@@ -14,9 +14,18 @@ extension Timer {
         switch interval {
             case .never: return nil
             default:
-               let timer = Timer(fire: Date() + interval.CC_timeInterval, interval: 0, repeats: false, block: block)
-               runloop.add(timer, forMode: mode)
-               return timer
+                let timer = Timer(fire: Date() + interval.CC_timeInterval, interval: 0, repeats: false, block: block)
+                runloop.add(timer, forMode: mode)
+                return timer
         }
+    }
+
+    /// Returns a ``Timer``, scheduled to fire exactly at the specified `deadline`.
+    /// If you're not supplying a ``RunLoop`` argument, we're using the one for the current ``Thread``.
+    public static func CC_oneShot(deadline: Date?, runloop: RunLoop = .current, mode: RunLoop.Mode = .common, block: @escaping @Sendable (Timer) -> Void) -> Timer? {
+        guard let deadline else { return nil }
+        let timer = Timer(fire: deadline, interval: 0, repeats: false, block: block)
+        runloop.add(timer, forMode: mode)
+        return timer
     }
 }
