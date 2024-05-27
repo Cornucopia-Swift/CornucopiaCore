e872cf6e5e8c86691f834d0fb7d9c0b9952caa31
Dr. Michael Lauer <mickey@vanille-media.de>
2024-05-27 19:27:06 +0200

    Extensions/DispatchTimeInterval: Add DispatchTimeInterval+Comparable

---
+++ Sources/CornucopiaCore/Extensions/DispatchTimeInterval/DispatchTimeInterval+Comparable.swift
@@ -0,0 +1,20 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+import Foundation
+
+extension DispatchTimeInterval: Comparable {
+
+    var CC_nanoseconds: UInt64 {
+        switch self {
+            case let .seconds(s):       UInt64(s)  * UInt64(1_000_000_000)
+            case let .milliseconds(ms): UInt64(ms) * UInt64(1_000_000)
+            case let .microseconds(ms): UInt64(ms) * UInt64(1000)
+            case let .nanoseconds(ns):  UInt64(ns)
+            case .never:                UInt64.max
+            @unknown default:           fatalError("Unknown value: \(self)")
+        }
+    }
+
+    public static func < (lhs: DispatchTimeInterval, rhs: DispatchTimeInterval) -> Bool { lhs.CC_nanoseconds < rhs.CC_nanoseconds }
+}
