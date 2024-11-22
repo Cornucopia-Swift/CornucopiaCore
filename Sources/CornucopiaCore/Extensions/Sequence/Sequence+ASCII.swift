94d98a07d209b3e2beb2dde16fa6017c176263c6
Dr. Michael Lauer <mickey@vanille-media.de>
2024-11-22 10:58:57 +0100

    Sequence+ASCII: deprecate in favor of radix string interpolation

--- Sources/CornucopiaCore/Extensions/Sequence/Sequence+ASCII.swift
+++ Sources/CornucopiaCore/Extensions/Sequence/Sequence+ASCII.swift
@@ -1,9 +1,9 @@
 //
 //  Cornucopia – (C) Dr. Lauer Information Technology
 //
+@available(*, deprecated, message: "Use radix string interpolation instead")
 public extension Sequence where Element == UInt8 {
 
     /// Returns an ASCII decoded string.
-    //FIXME: Shouldn't this rather be solved via a (customizable) string interpolation?
     var CC_asciiDecodedString: String { self.map { String(format: "%c", $0 > 0x1F && $0 < 0x80 ? $0 : 0x2E) }.joined() }
 }
