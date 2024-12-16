77fd0cc2280ef95b729f7d3e987c4c69fa56ea2c
Dr. Michael Lauer <mickey@vanille-media.de>
2024-12-16 12:08:28 +0100

    String+ContentValidation: add checks for IP address format

--- Sources/CornucopiaCore/Extensions/String/String+ContentValidation.swift
+++ Sources/CornucopiaCore/Extensions/String/String+ContentValidation.swift
@@ -8,10 +8,12 @@ public extension String {
     private static var PhoneRegexSource: String = "^((\\+)|(00))[0-9]{6,14}|[0-9]{6,14}$"
     private static var PhoneRegex: NSRegularExpression = try! .init(pattern: Self.PhoneRegexSource, options: [.caseInsensitive])
 
+    /// Checks if the string is a valid phone number.
     var CC_isValidPhoneNumber: Bool {
         Self.PhoneRegex.firstMatch(in: self, options: [], range: self.CC_nsRange) != nil
     }
 
+    /// Checks if the string is a valid eMail address.
     var CC_isValidEmailAddress: Bool {
         #if canImport(ObjectiveC)
         let link = "mailto:" + self
@@ -28,5 +30,23 @@ public extension String {
         return true
         #endif
     }
+    
+    /// Checks if the string is a valid IPv4 address.
+    var CC_isValidIPv4: Bool {
+        var sin = sockaddr_in()
+        return self.withCString { cString in
+            inet_pton(AF_INET, cString, &sin.sin_addr)
+        } == 1
+    }
+    
+    /// Checks if the string is a valid IPv6 address.
+    var CC_isValidIPv6: Bool {
+        var sin6 = sockaddr_in6()
+        return self.withCString { cString in
+            inet_pton(AF_INET6, cString, &sin6.sin6_addr)
+        } == 1
+    }
+    
+    /// Convenience check for both IP address formats.
+    var CC_isValidIPAddress: Bool { self.CC_isValidIPv4 || self.CC_isValidIPv6 }
 }
-
