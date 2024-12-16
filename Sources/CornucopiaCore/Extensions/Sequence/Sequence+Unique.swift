eb70b526c454f0085c018dcf13847eec05f9e0a2
Dr. Michael Lauer <mickey@vanille-media.de>
2024-12-16 13:36:58 +0100

    docs++

--- Sources/CornucopiaCore/Extensions/Sequence/Sequence+Unique.swift
+++ Sources/CornucopiaCore/Extensions/Sequence/Sequence+Unique.swift
@@ -2,8 +2,9 @@
 //  Cornucopia – (C) Dr. Lauer Information Technology
 //
 
-/// Returns an array where the elements are unique, preserving the original order.
 public extension Sequence where Iterator.Element: Hashable {
+
+    /// Returns an array where the elements are unique, preserving the original order.
     func CC_unique() -> [Iterator.Element] {
         var seen: [Iterator.Element: Bool] = [:]
         return self.filter { seen.updateValue(true, forKey: $0) == nil }
