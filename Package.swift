ba764bf33a75138557ce51657aa88e0303b74d77
Dr. Michael Lauer <mickey@vanille-media.de>
2025-03-03 12:40:36 +0100

    Package: Bump and add note about zmian's fork of AnyCodable

--- Package.swift
+++ Package.swift
@@ -4,10 +4,11 @@ import PackageDescription
 let package = Package(
     name: "CornucopiaCore",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v12),
-        .tvOS(.v15),
-        .watchOS(.v8),
+        .iOS(.v16),
+        .macOS(.v13),
+        .macCatalyst(.v13),
+        .tvOS(.v16),
+        .watchOS(.v9),
         //.linux
     ],
     products: [
@@ -16,8 +17,9 @@ let package = Package(
             targets: ["CornucopiaCore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto", .upToNextMajor(from: "3.0.0")),
-        .package(url: "https://github.com/tsolomko/SWCompression", .upToNextMajor(from: "4.8.5")),
+        .package(url: "https://github.com/apple/swift-crypto", .upToNextMajor(from: "3.11.0")),
+        .package(url: "https://github.com/tsolomko/SWCompression", .upToNextMajor(from: "4.8.6")),
+        // NOTE: When upgrading to Swift 6, move to zmian's fork @ https://github.com/zmian/AnyCodable
         .package(url: "https://github.com/mickeyl/AnyCodable", .upToNextMajor(from: "0.6.6")),
     ],
     targets: [
