60a56b0b8ca1f3bf9a55d2a5d2d7ad5b632a37b2
Dr. Michael Lauer <mickey@vanille-media.de>
2025-04-04 16:43:10 +0200

    Restore compilation w/ Swift 5.10

--- Sources/CornucopiaCore/Extensions/CoreLocation/CLAuthorizationStatus+CustomStringConvertible.swift
+++ Sources/CornucopiaCore/Extensions/CoreLocation/CLAuthorizationStatus+CustomStringConvertible.swift
@@ -4,8 +4,9 @@
 #if canImport(CoreLocation)
 import CoreLocation
 
-extension CLAuthorizationStatus: @retroactive CustomStringConvertible {
+extension CLAuthorizationStatus: CustomStringConvertible {
 
+    /// A textual representation of the authorization status.
     public var description: String {
         switch self {
             case .authorizedAlways:    ".authorizedAlways"
