86a8ae8ee13f432751a9e2b01dea1aea3cbf0f6d
Dr. Michael Lauer <mickey@vanille-media.de>
2025-11-01 15:58:06 +0100

    Concurrency: add another calling from to AsyncWithTimeout

--- Sources/CornucopiaCore/Features/Concurrency/AsyncWithTimeout.swift
+++ Sources/CornucopiaCore/Features/Concurrency/AsyncWithTimeout.swift
@@ -31,3 +31,29 @@ public func CC_asyncWithTimeout<ResultType: Sendable>(seconds: TimeInterval, bod
         return result
     }
 }
+
+/// Performs the work in `body`, but cancels the task if it takes longer than the specified amount of `seconds`.
+/// This variant accepts non-Sendable closures for use with actor-isolated code.
+public func CC_asyncWithTimeout<ResultType>(seconds: TimeInterval, body: @escaping () async throws -> ResultType) async throws -> ResultType {
+
+    try await withThrowingTaskGroup(of: ResultType.self) { group in
+
+        let deadline = Date(timeIntervalSinceNow: seconds)
+
+        // add actual work
+        group.addTask { try await body() }
+
+        // add timeout
+        group.addTask {
+            let interval = deadline.timeIntervalSinceNow
+            if interval > 0 {
+                try await Task.CC_sleep(seconds: interval)
+            }
+            try Task.checkCancellation()
+            throw Cornucopia.Core.AsyncWithTimeoutError()
+        }
+        let result = try await group.next()!
+        group.cancelAll()
+        return result
+    }
+}
